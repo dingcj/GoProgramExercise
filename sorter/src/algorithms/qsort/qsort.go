271bb09c86e082f603f6a44a12d06b595202c4d2
dingcongjun <dingcongjun@gmail.com>
2018-08-23 14:18:29 +0000

    添加快速排序，并实现sorter

--- sorter/src/algorithms/qsort/qsort.go
+++ sorter/src/algorithms/qsort/qsort.go
@@ -1,7 +1,37 @@
-package main
+package qsort
 
-import "fmt"
+func quickSort(values []int, left, right int) {
+	key := values[left]
+	p := left
+	i, j := left, right
 
-func main() {
-	fmt.Println("vim-go")
+	for i < j {
+		for j > p && values[j] >= key {
+			j--
+		}
+		if j > p {
+			values[p] = values[j]
+			p = j
+		}
+
+		for i < p && values[i] <= key {
+			i++
+		}
+		if i < p {
+			values[p] = values[i]
+			p = i
+		}
+	}
+	values[p] = key
+
+	if p-left > 1 {
+		quickSort(values, left, p-1)
+	}
+	if right-p > 1 {
+		quickSort(values, p+1, right)
+	}
+}
+
+func QuickSort(values []int) {
+	quickSort(values, 0, len(values)-1)
 }
--- sorter/src/sorter/sorter.go
+++ sorter/src/sorter/sorter.go
@@ -1,16 +1,94 @@
 package main
 
+import "algorithms/qsort"
+import "bufio"
 import "flag"
 import "fmt"
+import "io"
+import "os"
+import "strconv"
 
 var infile *string = flag.String("i", "unsorted.dat", "File contains values for sorting.")
 var outfile *string = flag.String("o", "sorted.dat", "File to receive sorted values.")
 var algorithm *string = flag.String("a", "bubblesort", "Sort algorithm")
 
+func readValues(infile string) (values []int, err error) {
+	file, oerr := os.Open(infile)
+	if oerr != nil {
+		fmt.Println("Failed to open input file ", oerr)
+		err = oerr
+		return
+	}
+	defer file.Close()
+
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+
+	for {
+		line, isPrefix, rerr := br.ReadLine()
+		if rerr != nil {
+			if rerr != io.EOF {
+				err = rerr
+			}
+			break
+		}
+
+		if isPrefix {
+			fmt.Println("A too long line, seems unexpected.")
+			break
+		}
+
+		str := string(line)
+
+		value, cerr := strconv.Atoi(str)
+		if cerr != nil {
+			err = cerr
+			break
+		}
+		values = append(values, value)
+	}
+
+	return
+}
+
+func writeValues(values []int, outfile string) error {
+	file, err := os.Create(outfile)
+	if err != nil {
+		fmt.Println("Failed to create the output file ", outfile)
+		return err
+	}
+	defer file.Close()
+
+	for _, value := range values {
+		str := strconv.Itoa(value)
+		file.WriteString(str + "\n")
+	}
+
+	return nil
+}
+
 func main() {
 	flag.Parse()
 
-	fmt.Println(*infile)
-	fmt.Println(*outfile)
-	fmt.Println(*algorithm)
+	values, err := readValues(*infile)
+	if err != nil {
+		fmt.Println("Read values from infile failed.")
+		return
+	}
+	fmt.Println("Success to read values from infile ", *infile)
+
+	switch *algorithm {
+	case "qsort":
+		fmt.Println("Use quick sort. Begin to sort.")
+		qsort.QuickSort(values)
+	case "bubblesort":
+		fmt.Println("Use bubble sort. Begin to sort.")
+		qsort.QuickSort(values)
+	default:
+		fmt.Println("Unknown algorithm.")
+		return
+	}
+
+	fmt.Println("The sort has been completed. Begin to write to file ", *outfile)
+	writeValues(values, *outfile)
 }
