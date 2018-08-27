80ec5be95933d423d97b21d6711545cfe5c1115d
dingcongjun <dingcongjun@gmail.com>
2018-08-27 09:15:07 +0800

    实现完整播放功能

--- smp/src/mplayer/mplayer.go
+++ smp/src/mplayer/mplayer.go
@@ -1,12 +1,12 @@
 package main
 
+import "bufio"
 import "fmt"
 import "library"
+import "mp"
+import "os"
 import "strconv"
-
-func main() {
-	fmt.Println("vim-go")
-}
+import "strings"
 
 var lib *library.MusicManager
 var id int = 1
@@ -37,5 +37,51 @@ func handleLibCommands(tokens []string) {
 		}
 	default:
 		fmt.Println("Unrecognized lib command: ", tokens[1])
+	}
+}
+
+func handlePlayCommand(tokens []string) {
+	if len(tokens) != 2 {
+		fmt.Println("USAGE: play <name>")
+		return
+	}
+
+	e := lib.Find(tokens[1])
+	if e == nil {
+		fmt.Println("This music ", tokens[1], " does not exist!")
+		return
+	}
+
+	mp.Play(e.Source, e.Type)
+}
+
+func main() {
+	fmt.Println(`
+	Enter following commands to control the player:
+	lib list -- View the existing music lib
+	lib add <name><artist><source><type> -- Add a new music to the music lib
+	lib remove <name> -- Remove the specified music from the lib
+	play name -- Play the specified music`)
+
+	lib = library.NewMusicManager()
+	r := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Enter command->")
+		rawLine, _, _ := r.ReadLine()
+		line := string(rawLine)
+
+		if line == "q" || line == "e" {
+			break
+		}
+
+		tokens := strings.Split(line, " ")
+		if tokens[0] == "lib" {
+			handleLibCommands(tokens)
+		} else if tokens[0] == "play" {
+			handlePlayCommand(tokens)
+		} else {
+			fmt.Println("Unrecognized commands: ", tokens[0])
+		}
 	}
 }
